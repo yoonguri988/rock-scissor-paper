86478a4f108972e26fa544c788844e5934da31e0
yoonguri988 <56820897+yoonguri988@users.noreply.github.com>
2024-11-10 23:46:06 +0900

    Feat: hand
    Box 컴포넌트에 Hand 컴포넌트 추가
    Hand 컴포넌트와 CSS 파일 추가

---
+++ src/Hand.js
@@ -0,0 +1,21 @@
+import "./Hand.css";
+import rockImg from "./assets/rock.svg";
+import scissorImg from "./assets/scissor.svg";
+import paperImg from "./assets/paper.svg";
+
+const ROCK_SCISSOR_PAPER = {
+  rock: rockImg,
+  scissor: scissorImg,
+  paper: paperImg,
+};
+function Hand({ name, className = "" }) {
+  const classNames = `Hand ${className}`;
+  const src = ROCK_SCISSOR_PAPER[name];
+  return (
+    <div class={classNames}>
+      <img class="Hand-icon" src={src} alt={name} />
+    </div>
+  );
+}
+
+export default Hand;
